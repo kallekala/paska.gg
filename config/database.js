029531e07c16b7b2005594b10e6cde2425aedfef
kallekala <ville.herlin@insead.edu>
2018-03-27 01:24:30 +0800

    second  commit

--- config/database.js
+++ config/database.js
@@ -3,9 +3,3 @@ if(process.env.NODE_ENV === 'production'){
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/fc3-dev'}
 }
-
-// if(process.env.NODE_ENV === 'production'){
-//     module.exports = {mongoURI: 'mongodb://CHANGEME'}
-//   } else {
-//     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
-//   }
