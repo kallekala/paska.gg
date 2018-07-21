9d182f9307e884095056fd21b3d5f97282fdbf06
kallekala <ville.herlin@insead.edu>
2018-07-21 14:55:09 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,11 @@
 // module.exports = {
-//     mongoURI:'mongodb://kallekala:salasana@ds223009.mlab.com:23009/paska-prod',
+//     mongoURI:'mongodb://kallekala:***@ds223009.mlab.com:23009/paska-prod',
 //     googleClientID: '277191687845-cjot2n8iev8rpk97va67ha8cok1g6emn.apps.googleusercontent.com',
 //     googleClientSecret: '0ZVx1wbHe8gnUgkMnblhl-2u'
 // }
 
 // if(process.env.NODE_ENV === 'production'){
-//     module.exports = {mongoURI: 'mongodb://kallekala:salasana@ds223009.mlab.com:23009/paska-prod'}
+//     module.exports = {mongoURI: 'mongodb://kallekala:***@ds223009.mlab.com:23009/paska-prod'}
 // } else {
 //     module.exports = {mongoURI: 'mongodb://localhost/fc3-dev'}
 // }
